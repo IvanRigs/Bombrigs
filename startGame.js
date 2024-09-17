17462f2d365c469967dfbab93d378dc9134201e4
IvanRigs <110514271+IvanRigs@users.noreply.github.com>
2024-09-17 00:32:38 -0600

    v6.3

--- startGame.js
+++ startGame.js
@@ -7,7 +7,7 @@ class Item {
         this.height = height;
         this.img = new Image(); 
         this.img.src = imagePath;
-        this.speed = 5;
+        this.speed = 7;
         this.animationClockX = 0;
         this.animationClockXTwo = 0;
         this.animationSpeed = 200;
@@ -35,8 +35,12 @@ class Item {
 // Variables --------------------------------------------------------
 const battletheme = new Audio("./assets/sounds/battletheme.mp3");
 battletheme.loop = true;
+battletheme.volume = 0.6;
 battletheme.play();
 var time0;
+var time1;
+
+const steps = new Audio("./assets/sounds/Walking1.wav");
 
 const canvasScreenX = ((canvas.width/2)-((canvas.height/2) + (canvas.height + 36)) / 2);
 const canvasScreenY = 0;
@@ -44,11 +48,50 @@ const canvasScreenW = ((canvas.height) + (canvas.height/2)) + 36;
 const canvasScreenH = canvas.height;
 
 const floor = new Item(0, 0, 48, 48, "./assets/battleOne/pasto.png");
-const floor2 = new Item(48, 96, 48, 48, "./assets/battleOne/pasto2.png");
 
-const wall1 = new Item(48, 48, 48, 48, "./assets/battleOne/wall1.png");
+//barreras
+const wallPath = "./assets/battleOne/wall1.png";
+const walls = [];
 
-const player = new Item(144, 22, 48, 74, "./assets/battleOne/bomb1.png");
+// Crear borde izquierdo
+for (var i = 48; i <= 624; i += 48) {
+    walls.push(new Item(48, i, 48, 48, wallPath)); // columna izquierda
+}
+
+// Crear borde derecho
+for (var i = 48; i <= 624; i += 48) {
+    walls.push(new Item(912, i, 48, 48, wallPath)); // columna derecha
+}
+
+// Crear borde arriba
+for (var i = 96; i <= 912; i += 48) {
+    walls.push(new Item(i, 48, 48, 48, wallPath)); // columna derecha
+}
+
+// Crear borde abajo
+for (var i = 96; i <= 912; i += 48) {
+    walls.push(new Item(i, 624, 48, 48, wallPath)); // columna derecha
+}
+
+// Crear wall dentro
+for (var i = 144; i <= 528; i += 96) {
+    for (var z = 144; z <= 816; z += 96) {
+        walls.push(new Item(z, i, 48, 48, wallPath)); // columna derecha
+    }
+}
+
+// Pasto2
+// Crear pasto dentro
+const floor2Path = "./assets/battleOne/pasto2.png";
+const floor2 = [];
+for (var i = 192; i <= 576; i += 96) {
+    for (var z = 144; z <= 816; z += 96) {
+        floor2.push(new Item(z, i, 48, 48, floor2Path)); // columna derecha
+    }
+}
+
+// Player
+const player = new Item(97, 60, 38, 64, "./assets/battleOne/bomb1.png");
 //player animaciones PATH
 var plyerA = ["assets/battleOne/bomb1.png", "assets/battleOne/bomb2.png", "assets/battleOne/bomb3.png", "assets/battleOne/bomb4.png", "assets/battleOne/bomb5.png", "assets/battleOne/bomb6.png", "assets/battleOne/bomb7.png", "assets/battleOne/bomb8.png", "assets/battleOne/bomb9.png", "assets/battleOne/bomb10.png", "assets/battleOne/bomb11.png", "assets/battleOne/bomb12.png"];
 
@@ -72,27 +115,47 @@ function drawStartGame() {
         }
     }
 
-    wall1.draw(ctx);
-    floor2.draw(ctx);
+    //floor2.draw(ctx);
    
-    while (wall1.collision(player)) {
-        switch (player.direction) {
-            case 'up':
-                player.y = player.y + 1;
+    walls.forEach(function(wall, i, array){
+        wall.draw(ctx);
+        while (wall.collision(player)) {
+            switch (player.direction) {
+                case 'up':
+                    player.y = player.y + 1;
+                    break;
+                case 'down':
+                    player.y = player.y - 1;
+                    break;
+                case 'right':
+                    player.x = player.x - 1;
                 break;
-            case 'down':
-                player.y = player.y - 1;
-                break;
-            case 'right':
-                player.x = player.x - 1;
-            break;
-            case 'left':
-                player.x = player.x + 1;
-            break;       
+                case 'left':
+                    player.x = player.x + 1;
+                break;       
+            }
         }
-    }
+    });
 
-    player.draw(ctx);
+    floor2.forEach(function(floors, i, array){
+        floors.draw(ctx);
+    });
+
+    if ((player.img.src.includes(plyerA[0]) ||
+    player.img.src.includes(plyerA[3]) ||
+    player.img.src.includes(plyerA[6]) ||
+    player.img.src.includes(plyerA[9])  ||
+    player.img.src.includes("assets/battleOne/homer1.png") ||
+    player.img.src.includes("assets/battleOne/homer2.png") ||
+    player.img.src.includes("assets/battleOne/homer3.png") ||
+    player.img.src.includes("assets/battleOne/homer4.png") ||
+    player.img.src.includes("assets/battleOne/homer5.png") ||
+    player.img.src.includes("assets/battleOne/homer6.png")) && (performance.now() - time0 > 10000)) {
+        homer();
+        player.draw(ctx);
+    }else {
+        player.draw(ctx);
+    }
 
     animationFrameId = requestAnimationFrame(drawStartGame);
 }
@@ -132,29 +195,58 @@ function controlsSG(event) {
     }
 }
 
+function homer() {
+    if (performance.now() - time0 <= 10000){
+        player.img.src = "assets/battleOne/homer1.png"
+    }else if (performance.now() - time0 <= 10100){
+        player.width = 45;
+        player.img.src = "assets/battleOne/homer2.png"
+    }else if ((performance.now() - time0 <= 10200) || (performance.now() - time1 <= 100)){
+        player.width = 38;
+        player.img.src = "assets/battleOne/homer3.png"
+    }else if ((performance.now() - time0 <= 10300) || (performance.now() - time1 <= 200)){
+        player.img.src = "assets/battleOne/homer4.png"
+    }else if ((performance.now() - time0 <= 10400) || (performance.now() - time1 <= 300)){
+        player.img.src = "assets/battleOne/homer5.png"
+    }else if ((performance.now() - time0 <= 10500) || (performance.now() - time1 <= 400)){
+        player.img.src = "assets/battleOne/homer6.png"
+        time1 = performance.now();
+    }
+    
+}
+
 // Animacion
 function playerAnimation(aO, aT) {
-    var time1 = performance.now();
+    var time2 = performance.now();
     if (player.img.src.includes(plyerA[0]) ||
     player.img.src.includes(plyerA[3]) ||
     player.img.src.includes(plyerA[6]) ||
-    player.img.src.includes(plyerA[9])) {
+    player.img.src.includes(plyerA[9]) ||
+    player.img.src.includes("assets/battleOne/homer1.png") ||
+    player.img.src.includes("assets/battleOne/homer2.png") ||
+    player.img.src.includes("assets/battleOne/homer3.png") ||
+    player.img.src.includes("assets/battleOne/homer4.png") ||
+    player.img.src.includes("assets/battleOne/homer5.png") ||
+    player.img.src.includes("assets/battleOne/homer6.png")) {
+        player.width = 38;
+        player.height = 64;
         player.img.src = plyerA[aO];
         time0 = performance.now();
+        steps.play();
     } else if (player.img.src.includes(plyerA[aO]) &&
-     (time1 - time0) >= player.animationSpeed) {
+     (time2 - time0) >= player.animationSpeed) {
         player.img.src = plyerA[aT];
         time0 = performance.now();
-        
+        steps.play();
     } else if (player.img.src.includes(plyerA[aT]) &&
-    (time1 - time0) >= player.animationSpeed) {
+    (time2 - time0) >= player.animationSpeed) {
         player.img.src = plyerA[aO];
         time0 = performance.now();
+        steps.play();
     }
 }
 
 function backPlayer() {
-    console.log("BPPPPPPPPPPPPPPP")
     if (player.img.src.includes(plyerA[1]) ||
     player.img.src.includes(plyerA[2])){
         player.img.src = plyerA[0];
@@ -168,6 +260,8 @@ function backPlayer() {
     player.img.src.includes(plyerA[11])){
         player.img.src = plyerA[9];
     }
+    console.log("zsdadzz")
+    time0 = performance.now();
 }
 
 // Eventos ----------------------------------------------------------
